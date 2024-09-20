af049559f50b0390183efc71cd00493ae5745678
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-09-19 22:33:32 -0300

    feat: add a Servlet that always returns 'hello' on a '/greeting' route

---
+++ deployment/target/generated-sources/annotations/org/acme/greeting/extension/deployment/GreetingExtensionProcessor$$accessor.java
@@ -0,0 +1,8 @@
+package org.acme.greeting.extension.deployment;
+@io.quarkus.Generated("Quarkus annotation processor")
+public final class GreetingExtensionProcessor$$accessor {
+    private GreetingExtensionProcessor$$accessor() {}
+    public static Object construct() {
+        return new GreetingExtensionProcessor();
+    }
+}
